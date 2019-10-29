48c27c9f463381efbd227ef534cb63164d177e06
Obed Tetteh <obed.tetteh@ibm.com>
2019-10-29 14:18:45 +0100

    first commit

---
+++ adder/adder.go
@@ -0,0 +1,6 @@
+package adder
+
+// Add return the sum of the two given integers
+func Add(n1 int, n2 int) int {
+	return n1 + n2
+}
---
+++ adder/adder_test.go
@@ -0,0 +1,18 @@
+package adder_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/ObedTetteh/ginkgotest/adder"
+)
+
+var _ = Describe("Adder", func() {
+	Describe("Add", func() {
+
+		It("adds two numbers", func() {
+			sum := Add(2, 3)
+			Expect(sum).To(Equal(5))
+		})
+	})
+})
